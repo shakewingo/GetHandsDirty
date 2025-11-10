706d65b3f0b191e20f504d3902e1baa0e38d48fb
Ying Yao <yyao374@gatech.edu>
2025-11-10 20:39:19 +0800

    clean old files

--- pfa/ios/FinancialTracker/FinancialTracker/Models/AccountSummary.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct AccountSummary: Codable {
-    let totalAssets: Double
-    let totalCredit: Double
-    let netWorth: Double
-    let monthlySummary: [String: [String: Double]]
-}
